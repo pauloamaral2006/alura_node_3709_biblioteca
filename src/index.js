7760405a865fd9066fdb60391a7ed93a9e720d49
Paulo Amaral <pauloamaral2006@yahoo.com.br>
2026-04-09 11:05:04 -0300

    Aula 02

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2]; 
+const link = caminhoArquivo[2];
+function quebraEmParagrafos(texto) {
+  const paragrafos = texto.toLowerCase().split("\n");
+  const contagem = paragrafos.reduce((acum, paragrafo) => {
+    if (paragrafo) {
+      return [...acum, verificaPalavrasDuplicadas(paragrafo)];
+    }
+    return acum;
+  }, []);
+  console.log(contagem);
+}
 
-fs.readFile(link, 'utf-8', (erro, texto) => {
-  console.log(texto);
-})
+function limpaPalavras(palavra) {
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+}
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(" ");
+  const resultado = {};
+  // objeto[propriedade] = valor;
+  listaPalavras.forEach((palavra) => {
+    if (palavra.length >= 3) {
+      const palavraLimpa = limpaPalavras(palavra);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+    }
+  });
+  return resultado;
+}
+
+fs.readFile(link, "utf-8", (erro, texto) => {
+  quebraEmParagrafos(texto);
+});
